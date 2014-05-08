3c14dc420a34b84dcbb569c1cfd8eeb3b4fb348a
chencheng <sorrycc@gmail.com>
2014-05-08 15:45:44 +0800

    transform to commonjs

--- src/base.js
+++ src/base.js
@@ -1,52 +1,49 @@
-define(function(require, exports, module) {
+// Base
+// ---------
+// Base 是一个基础类，提供 Class、Events、Attrs 和 Aspect 支持。
 
-  // Base
-  // ---------
-  // Base 是一个基础类，提供 Class、Events、Attrs 和 Aspect 支持。
-
-  var Class = require('class');
-  var Events = require('events');
-  var Aspect = require('./aspect');
-  var Attribute = require('./attribute');
+var Class = require('class');
+var Events = require('events');
+var Aspect = require('./aspect');
+var Attribute = require('./attribute');
 
 
-  module.exports = Class.create({
-    initialize: function(config) {
-      this.initAttrs(config);
+module.exports = Class.create({
+  initialize: function(config) {
+    this.initAttrs(config);
 
-      // Automatically register `this._onChangeAttr` method as
-      // a `change:attr` event handler.
-      parseEventsFromInstance(this, this.attrs);
-    },
+    // Automatically register `this._onChangeAttr` method as
+    // a `change:attr` event handler.
+    parseEventsFromInstance(this, this.attrs);
+  },
 
-    destroy: function() {
-      this.off();
+  destroy: function() {
+    this.off();
 
-      for (var p in this) {
-        if (this.hasOwnProperty(p)) {
-          delete this[p];
-        }
+    for (var p in this) {
+      if (this.hasOwnProperty(p)) {
+        delete this[p];
       }
-
-      // Destroy should be called only once, generate a fake destroy after called
-      // https://github.com/aralejs/widget/issues/50
-      this.destroy = function() {};
     }
-  }).implement([Events, Aspect, Attribute]);
+
+    // Destroy should be called only once, generate a fake destroy after called
+    // https://github.com/aralejs/widget/issues/50
+    this.destroy = function() {};
+  }
+}).implement([Events, Aspect, Attribute]);
 
 
-  function parseEventsFromInstance(host, attrs) {
-    for (var attr in attrs) {
-      if (attrs.hasOwnProperty(attr)) {
-        var m = '_onChange' + ucfirst(attr);
-        if (host[m]) {
-          host.on('change:' + attr, host[m]);
-        }
+function parseEventsFromInstance(host, attrs) {
+  for (var attr in attrs) {
+    if (attrs.hasOwnProperty(attr)) {
+      var m = '_onChange' + ucfirst(attr);
+      if (host[m]) {
+        host.on('change:' + attr, host[m]);
       }
     }
   }
+}
 
-  function ucfirst(str) {
-    return str.charAt(0).toUpperCase() + str.substring(1);
-  }
-});
+function ucfirst(str) {
+  return str.charAt(0).toUpperCase() + str.substring(1);
+}
